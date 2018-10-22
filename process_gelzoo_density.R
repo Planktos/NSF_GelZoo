1b31186589f3bc16f70bb192b5a95d3d05154ea8
plantkos <kellylynnrobinson@gmail.com>
2018-10-22 18:20:33 -0500

    added summary statistics

--- process_gelzoo_density.R
+++ process_gelzoo_density.R
@@ -5,3 +5,18 @@
 #Date Created: 22 October 2018
 #Date Last Modified: 22 October 2018
 
+#libraries
+library(plyr)
+library(dplyr)
+
+d <- read.csv("gel_RP1.csv", header = T, stringsAsFactors = F)
+
+d.sum.taxa <- d %>% group_by(taxa) %>% summarise(mean.density = mean(std.density), sd.density = sd(std.density), n = n())
+write.csv(x = d.sum.taxa, file = "GelZoo_Taxa_Stats.csv", row.names = F)
+
+d.sum.stn <- d %>% group_by(stn) %>% summarise(mean.density = mean(std.density), sd.density = sd(std.density), n = n())
+stn.coord <- unique(d[,c("stn","lat","lon")])
+d.sum.stn.coord <- join(d.sum.stn, stn.coord, "stn")
+write.csv(x = d.sum.stn.coord, file = "GelZoo_Stn_Stats.csv", row.names = F)
+
+
