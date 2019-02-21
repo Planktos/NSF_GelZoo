e503ffa6de558a1e9463d8726c0de08ee5bb2dd6
plantkos <kellylynnrobinson@gmail.com>
2019-02-21 12:04:01 -0600

    Added groups with zeros counts back into data using ddply

--- process_gelzoo_density.R
+++ process_gelzoo_density.R
@@ -19,10 +19,30 @@ d2$cruise <- toupper(substr(d2$sample_id, 1,3))
 
 d <- rbind(d1, d2)
 
-taxa.n <- unique(d$taxa)
 d$taxa<- gsub(d$taxa, pattern = "Ctenophora<Metazoa", replacement = "ctenophore")
 d$taxa<- gsub(d$taxa, pattern = "Ctenophora", replacement = "ctenophore")
 d$taxa <- tolower(d$taxa)
+
+#find missing groups and add them back in as zeros
+taxa.n <- as.data.frame(unique(d$taxa))
+colnames(taxa.n) <- "taxa"
+
+df <- ddply(d, .variables = c("cruise","stn","sample_id"), function(n){
+t <- merge(x = n, y = taxa.n, by = "taxa", all.y = T)
+t$cruise <- unique(n$cruise)
+t$stn <- unique(n$stn)
+t$sample_id <- unique(n$sample_id)
+t$lat <- unique(n$lat)
+t$lon <- unique(n$lon)
+t$std.density <- ifelse(is.na(t$std.density), 0, t$std.density)
+t$count <- ifelse(is.na(t$count), 0, t$count)
+
+return(t)
+
+}, .progress = "text", .inform = T)
+
+m <- merge(x = t, y = n1, by = c("taxa","cruise","stn"), all.x = T)
+
 
 d.sum.taxa <- d %>% group_by(cruise, taxa) %>% summarise(mean.density = mean(std.density), sd.density = sd(std.density), n = n())
 write.csv(x = d.sum.taxa, file = "GelZoo_Taxa_Stats.csv", row.names = F)
